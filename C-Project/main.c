b1eb504c62c5e1ca8937b6e05a6895fa1e96062a
wang542413041 <542413041@qq.com>
2017-03-01 17:01:43 +0800

    edit main.c file

--- C-Project/main.c
+++ C-Project/main.c
@@ -7,6 +7,7 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
 //声明，不是定义
 extern int i;
@@ -23,6 +24,25 @@ extern void write_extern();
 
 //全局变量
 static int count = 5;
+//全局变量
+int g;
+
+/*
+struct [structure tag]
+{
+    member definition;
+    member definition;
+    ...
+    member definition;
+} [one or more structure variables];
+ */
+//结构体
+struct Books {
+    char title[50];
+    char author[50];
+    char subject[100];
+    int book_id;
+} book;
 
 //---方法的声明---
 void baseLanguage(void);
@@ -32,6 +52,14 @@ void func(void);
 void useOtherFileMethodAndOtherUseCurrentParam(void);
 void arithmeticOperator(void);
 void logCValue(int value);
+void infiniteLoop(void);
+void cFunction(void);
+void localVariable(void);
+void arrayInfo(void);
+void adressAcess(void);
+void charInfo(void);
+void structFucn(void);
+void printBook(struct Books book);
 
 int main(int argc, const char * argv[]) {
     
@@ -50,7 +78,177 @@ int main(int argc, const char * argv[]) {
     //算数运算符方法调用
     arithmeticOperator();
     
+    //无限循环
+    infiniteLoop();
+    
+    //函数
+    cFunction();
+    
+    //局部变量
+    localVariable();
+    
+    //数组
+    arrayInfo();
+    
+    //访问地址、指针
+    adressAcess();
+    
+    //字符串
+    charInfo();
+    
+    //结构体
+    structFucn();
+    
     return 0;
+}
+
+//结构体作为函数参数
+void printBook(struct Books book) {
+    printf( "Book title : %s\n", book.title);
+    printf( "Book author : %s\n", book.author);
+    printf( "Book subject : %s\n", book.subject);
+    printf( "Book book_id : %d\n", book.book_id);
+}
+
+//结构体
+void structFucn(void) {
+    struct Books Book1;        /* 声明 Book1，类型为 Book */
+    struct Books Book2;        /* 声明 Book2，类型为 Book */
+    
+    /* Book1 详述 */
+    strcpy( Book1.title, "C Programming");
+    strcpy( Book1.author, "Nuha Ali");
+    strcpy( Book1.subject, "C Programming Tutorial");
+    Book1.book_id = 6495407;
+    
+    /* Book2 详述 */
+    strcpy( Book2.title, "Telecom Billing");
+    strcpy( Book2.author, "Zara Ali");
+    strcpy( Book2.subject, "Telecom Billing Tutorial");
+    Book2.book_id = 6495700;
+    
+    /* 输出 Book1 信息 */
+    printf( "Book 1 title : %s\n", Book1.title);
+    printf( "Book 1 author : %s\n", Book1.author);
+    printf( "Book 1 subject : %s\n", Book1.subject);
+    printf( "Book 1 book_id : %d\n", Book1.book_id);
+    
+    /* 输出 Book2 信息 */
+    printf( "Book 2 title : %s\n", Book2.title);
+    printf( "Book 2 author : %s\n", Book2.author);
+    printf( "Book 2 subject : %s\n", Book2.subject);
+    printf( "Book 2 book_id : %d\n", Book2.book_id);
+}
+
+//字符串
+void charInfo(void) {
+    char greeting0[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+    char greeting1[] = "Hello";
+    printf("Greeting0 message: %s\n", greeting0 );
+    printf("Greeting1 message: %s\n", greeting1 );
+    
+    char str1[12] = "Hello";
+    char str2[12] = "World";
+    char str3[12];
+    unsigned long int  len ;
+    
+    /* 复制 str1 到 str3 */
+    strcpy(str3, str1);
+    printf("strcpy( str3, str1) :  %s\n", str3 );
+    
+    /* 连接 str1 和 str2 */
+    strcat( str1, str2);
+    printf("strcat( str1, str2):   %s\n", str1 );
+    
+    /* 连接后，str1 的总长度 */
+    len = strlen(str1);
+    printf("strlen(str1) :  %lu\n", len );
+}
+
+//访问地址
+void adressAcess(void) {
+    int var1;
+    char var2[10];
+    printf("var1 变量的地址： %x\n", &var1);
+    printf("var2 地址 %x\n", &var2);
+    
+    int var = 20;
+    int *ip;
+    ip = &var;
+    printf("Address of var variable: %x\n", &var  );
+    /* 在指针变量中存储的地址 */
+    printf("Address stored in ip variable: %x\n", ip );
+    /* 使用指针访问值 */
+    printf("Value of *ip variable: %d\n", *ip );
+    
+    int  *ptr = NULL;
+    printf("ptr 的值是 %x\n", ptr  );
+}
+
+//数组
+void arrayInfo(void) {
+    //type arrayName [ arraySize ];
+    
+    //声明数组
+    int balance[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    double newBalance[] = {1000.0, 2.0, 3.4, 7.0, 50.0};
+    
+    balance[4] = 40;
+    int use = balance[4];
+    printf("%d", use);
+    
+    for (int i = 0; i < 5; i++) {
+        printf("%.2f\n", newBalance[i]);
+    }
+    
+}
+
+//局部变量
+void localVariable(void) {
+    
+    /*
+    数据类型	初始化默认值
+    int	0
+    char	'\0'
+    float	0
+    double	0
+    pointer	NULL
+     */
+    
+    //局部变量声明
+    int a, b;
+    int c;
+    
+    //初始化
+    a = 10;
+    b = 20;
+    c = a + b;
+    g = a + b;
+    
+    printf("a = %d ; b = %d ; c = %d ; g = %d\n", a, b, c, g);
+    
+}
+
+//函数:
+void cFunction(void) {
+    printf("我都已经用了这么多函数了，你还想让我说什么？\n");
+    
+    char info[] = "return_type function_name( parameter list ) {\n    body of the function\n}\n";
+    printf("%s", info);
+}
+
+//无限循环:C程序员偏向 ctrl + c 停止循环
+void infiniteLoop(void) {
+    int limit = 100;
+    for (; ; ) {
+        printf("无限循环 limit = %d \n", limit);
+        limit--;
+        if (limit == 0) {
+            return;
+        } else {
+            continue;
+        }
+    }
 }
 
 //调用其它文件方法，其它文件调用本文件参数
